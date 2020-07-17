384c03ba9651788a8fbf0f3b0b6d050004fcff90
Nuradin Ismail <nuradin170@gmail.com>
2020-07-17 20:16:56 +0300

    Sending JWT in a cookie, Auth Protect and middleware Role Authorization

--- routes/courses.js
+++ routes/courses.js
@@ -10,6 +10,8 @@ const Course = require('../models/Course');
 const advancedResults = require('../middleware/advancedResults');
 const router = express.Router({ mergeParams: true });
 
+const { protect, authorize } = require('../middleware/auth');
+
 router
   .route('/')
   .get(
@@ -19,7 +21,11 @@ router
     }),
     getCourses
   )
-  .post(addCourse);
-router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse);
+  .post(protect, authorize('publisher', 'admin'), addCourse);
+router
+  .route('/:id')
+  .get(getCourse)
+  .put(protect, authorize('publisher', 'admin'), updateCourse)
+  .delete(protect, authorize('publisher', 'admin'), deleteCourse);
 
 module.exports = router;
